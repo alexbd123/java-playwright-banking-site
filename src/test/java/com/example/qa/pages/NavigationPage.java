81e3767b672edb033c2365d4672d005e48ce42a1
Alex Berridge-Dunn <alex@example.com>
2026-01-25 16:31:28 +0000

    Added NavigationPage for site navigation

---
+++ src/test/java/com/example/qa/pages/NavigationPage.java
@@ -0,0 +1,61 @@
+package com.example.qa.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+
+public class NavigationPage {
+    private final Page page;
+    private final Locator openNewAccountLink;
+    private final Locator accountsOverviewLink;
+    private final Locator transferFundsLink;
+    private final Locator billPayLink;
+    private final Locator findTransactionsLink;
+    private final Locator updateContactInfoLink;
+    private final Locator requestLoanLink;
+    private final Locator registrationPageLink;
+
+    public NavigationPage(Page page) {
+        this.page = page;
+        this.openNewAccountLink = page.locator("//a[contains(text(),'Open New Account')]");
+        this.accountsOverviewLink = page.locator("//a[contains(text(),'Accounts Overview')]");
+        this.transferFundsLink = page.locator("//a[contains(text(),'Transfer Funds')]");
+        this.billPayLink = page.locator("//a[contains(text(),'Bill Pay')]");
+        this.findTransactionsLink = page.locator("//a[contains(text(),'Find Transactions')]");
+        this.updateContactInfoLink = page.locator("//a[contains(text(),'Update Contact Info')]");
+        this.requestLoanLink = page.locator("//a[contains(text(),'Request Loan')]");
+        this.registrationPageLink = page.locator("//a[contains(text(),'Register')]");
+    }
+
+    public void registrationPage() {
+        registrationPageLink.click();
+    }
+
+    public void openNewAccount() {
+        openNewAccountLink.click();
+    }
+
+    public void accountsOverview() {
+        accountsOverviewLink.click();
+    }
+
+    public void transferFunds() {
+        transferFundsLink.click();
+    }
+
+    public void billPay() {
+        billPayLink.click();
+    }
+
+    public void findTransactions() {
+        findTransactionsLink.click();
+    }
+
+    public void updateContactInfo() {
+        updateContactInfoLink.click();
+    }
+
+    public void requestLoan() {
+        requestLoanLink.click();
+    }
+}
