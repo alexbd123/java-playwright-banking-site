89da251b3ebb4cf24fdd4b280ab6abef51f06c16
Alex Berridge-Dunn <alex@example.com>
2026-01-28 16:17:13 +0000

    removed unused value from AccountTypes enums

--- src/main/java/com/example/qa/enums/AccountTypes.java
+++ src/main/java/com/example/qa/enums/AccountTypes.java
@@ -2,17 +2,14 @@ package com.example.qa.enums;
 
 
 public enum AccountTypes {
-    CHECKING("0", "CHECKING"),
-    SAVINGS("1", "SAVINGS");
+    CHECKING("CHECKING"),
+    SAVINGS("SAVINGS");
 
-    public final String value;
     public final String label;
 
-    AccountTypes(String value, String label) {
-        this.value = value;
+    AccountTypes(String label) {
         this.label = label;
     }
-    public String value() { return value; }
-    public String label() { return label; }
+    public String getLabel() { return label; }
 }
 
